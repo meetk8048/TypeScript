e40e5ad1763e91f168a1015925c970066dddd333
meetk <meetkathiriya80484@gmail.com>
2026-01-01 12:17:21 +0530

    Ex-2 explan ts

--- app.ts
+++ app.ts
@@ -1,11 +1,18 @@
 // let num1= 5;
+
 // function calculate(num1: number, num2: number) {
 //     return num1 + num2;
 // }
+
 // console.log(calculate(num1,3));
-function getTotal(numbers) {
-    return numbers.reduce(function (acc, item) {
+
+2
+function getTotal(numbers:Array<number>): number {
+    return numbers.reduce((acc, item) => {
         return acc + item;
     }, 0);
 }
+
 console.log(getTotal([1, 2, 3]));
+
+
